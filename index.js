8bdbb13d2941a33912ec5f777f2c6923b28cd61c
BaharMahmud <bahar.a.mahmud@gmail.com>
2024-01-24 16:35:55 -0500

    indes .js additions

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const generateReadMe=({title, description, installation, usage, contributing, tests,  license, gitHub, email})=>
-`# ${title}
-${description}`
-;
+`# README file for ${title} project \n
+![GitHub License] ${license} \n \n
+
+Description:\n
+${description}  \n \n \n
+
+## Table of contents \n
+
+*[Installation](#installation)
+
+
+ Installation : \n
+${installation} \n \n
+Usage : \n
+${usage} \n \n
+Contribution : \n
+${contributing} \n \n
+Test: \n
+${tests} \n \n
+
+Questions: \n
+If you have any questions about the repo, open an issue or contact me  directly at ${email}.You can find more 
+of my work at ${gitHub} 
+
+`;
+
+
 
 const inquirer = require('inquirer');
 const fs=require('fs');
@@ -46,7 +70,10 @@ name:"tests",
     type:'list',
 message:'Which lisence would you like to add to your README file.',
 name:"license",
-choices:["Public domain license",  "GNU Lesser General Public License", "Permissive license", "Copyleft license", "Proprietary license"],
+choices:["Apache 2.0",  "Boost Software License 1.0", "GNU FDL v1.3", "ISC License (ISC)", "The MIT License"],
+badges:["https://opensource.org/licenses/Apache-2.0","https://img.shields.io/badge/License-Boost_1.0-lightblue.svg",
+"https://img.shields.io/badge/License-FDL_v1.3-blue.svg","https://img.shields.io/badge/License-ISC-blue.svg","https://img.shields.io/badge/License-MIT-yellow.svg"]
+
 },
 
 {
@@ -63,7 +90,13 @@ name:"email",
 
 ])
 .then((response) =>{
-
+var respond=response.license;
+if(response.license==="Apache 2.0"){
+license="https://opensource.org/licenses/Apache-2.0";
+}
+if(response.license==="Boost Software License 1.0"){
+    license="https://img.shields.io/badge/License-Boost_1.0-lightblue.svg";
+    }
 const readMeFileContent=generateReadMe(response);
 fs.writeFile("README.md",readMeFileContent,(err)=>
 err ? console.log(err) : console.log ("README was created successfully")
